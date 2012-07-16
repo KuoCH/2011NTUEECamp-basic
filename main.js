8cfb127357211e60442cb889673089fa968a625f
KuoCH <Kuo@Mac>
2012-07-16 16:34:02 +0800

    剛剛挖空挖到原本的檔案了，所以main.js和student/main.js對調

--- main.js
+++ main.js
@@ -1,27 +1,14 @@
-function imageLoaded(){
-
+function imageLoaded(){  //此處挖空讓小隊員填空
+  modifyStatus('圖片載入完成！你有辦法完成這個拼圖嗎？');
+  recordImageInfo();            // 記錄Image的資訊
+  setContainerSize();           //設定container層的大小位置
+  setMapSize();                 //設定map層的大小位置
+  calculateDistributeRegion();  //計算碎片打亂位置的範圍
+  generatePieces();             //產生各個碎片
+  randomizePieces();            //將各個碎片打亂
 }
-function win(){
-
+function win(){ //這個方塊可挖空給小隊員填效果
+    console.log('YAYA~~~~~');
+    modifyStatus( '恭喜恭喜～你完成了！' );
+    alert('con');
 }
-//modifyStatus(s)
-//將傳入的 s 字串，取代 index.html 中 id 為 status 的 <p> 標籤內容。
-//
-//recordImageInfo()
-//記錄圖片的資訊，像是圖片的長跟高，以及切成拼圖碎片的長跟高。
-//
-//setContainerSize()
-//設定 container 這一層的大小，由圖片大小決定，由圖片的長與高乘以一定的比例後得到。
-//setMapSize()
-//設定 map 這一層的大小，由圖片大小決定。因為是要完成拼圖的位置，所以與圖片大小相同。
-//並將它的水平位置移到 container 的正中間。
-//
-//calculateDistributeRegion()
-//計算碎片一開始打亂後能放的位置範圍，網頁上是在 map 下面。
-//同時設定滑鼠可作用的範圍（也就是 container 的範圍）。
-//
-//24generatePieces()
-//「碎片產生機」。把每個碎片產生、顯示出來，並照順序放進 pieces 陣列。
-//
-//randomizePieces()
-//利用 calculateDistributeRegion 所算出的亂數範圍值，將 pieces裡的每一個碎片的位置亂擺在範圍內。

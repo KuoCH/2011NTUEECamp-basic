a848ac364d213d2e0432052ab5f4ec394a86220b
KuoCH <Kuo@Mac>
2012-07-16 16:29:30 +0800

    api和main分開

--- main.js
+++ main.js
@@ -1,227 +1,27 @@
-var URL='https://fbcdn-sphotos-a.akamaihd.net/hphotos-ak-ash3/s720x720/527073_472781902738329_1982279571_n.jpg';
-var image = new Image();
-image.src = URL;
-image.onload = imageLoaded
-var map = document.getElementById('map');
-var container = document.getElementById('container');
-var imageUrlInput = document.getElementById('image-url');
-var pContainer = document.getElementById('piecesContainer')
-var mapContainerHeightRatio = 1.8,mapContainerWidthRatio = 1.2;
-var entireHeight,entireWidth,pieceHeight,pieceWidth;
-var distribute={
-  t:0,
-  l:0,
-  h:0,
-  w:0,
-};
-var mouseRange={
-  t:0,
-  l:0,
-  h:0,
-  w:0,
-};
-var mapLeft;
-var ROWS = 3,COLS = 3;
-var pieces = [],inmap = [];
-var carriedPiece=null;
-var carriedMouseXOffset=0;
-var carriedMouseYOffset=0;
-var zCount=20;
-
-function imageLoaded(){  //此處挖空讓小隊員填空
-  modifyStatus('圖片載入完成！你有辦法完成這個拼圖嗎？');
-  recordImageInfo();            // 記錄Image的資訊
-  setContainerSize();           //設定container層的大小位置
-  setMapSize();                 //設定map層的大小位置
-  calculateDistributeRegion();  //計算碎片打亂位置的範圍
-  generatePieces();             //產生各個碎片
-  randomizePieces();            //將各個碎片打亂
-}
-function recordImageInfo(){
-  entireHeight=image.height;
-  entireWidth=image.width;
-  pieceHeight=Math.floor(entireHeight/ROWS);
-  pieceWidth=Math.floor(entireWidth/COLS);
-}
-function setContainerSize(){
-  container.style.height = Math.floor(entireHeight*mapContainerHeightRatio) + 'px';
-  container.style.width = Math.floor(entireWidth*mapContainerWidthRatio) + 'px';
-}
-function setMapSize(){
-  map.style.height = entireHeight + 'px';
-  map.style.width = entireWidth + 'px';
-  mapLeft = Math.floor((mapContainerWidthRatio-1)*entireWidth/2);
-  map.style.left = mapLeft + 'px';
-}
-function calculateDistributeRegion(){
-  distribute.t = entireHeight;
-  distribute.l = 0;
-  distribute.w = Math.floor(entireWidth*mapContainerWidthRatio-pieceWidth);
-  distribute.h = Math.floor((mapContainerHeightRatio-1)*entireHeight - pieceHeight);
-  mouseRange.t = container.offsetTop;
-  mouseRange.l = container.offsetLeft;
-  mouseRange.w = container.offsetWidth;
-  mouseRange.h = container.offsetHeight;
-}
-function generatePieces(){
-  if(pieces.length!==0){
-    pContainer.innerHTML ='';
-  }
-
-  for(var i=0;i<ROWS;i++){
-    for(var j=0;j<COLS;j++){
-      pieces[i*ROWS+j] = newPiece(i,j);
-      inmap[i*ROWS+j] = null;
-    }
-  }
-}
-
-function newPiece(r,c){
-  var piece = document.createElement('div');
-  if(r === (ROWS-1)){
-    piece.style.height = entireHeight-(r)*pieceHeight + 'px';
-  }else{
-    piece.style.height = pieceHeight + 'px';
-  }
-  if(c === (COLS-1)){ 
-    piece.style.width = entireWidth-(c)*pieceWidth + 'px';
-  }else{
-    piece.style.width = pieceWidth + 'px';
-  }
-  piece.style.position = 'absolute';
-  piece.style.overflow = 'hidden';
-  piece.style.zIndex = zCount++;
-  piece.innerHTML = '<img src="' + URL + '">';
-  var img = piece.firstChild;
-  img.style.position = 'relative'
-  img.style.top = (-r)*pieceHeight+'px';
-  img.style.left = (-c)*pieceWidth+'px';
-  img.ondragstart = function(){ return false;  }
-  pContainer.appendChild(piece);
-  return {
-    elm:piece,
-    seq:r*ROWS+c,
-    oInmap:null,
-    oX:null,
-    oY:null
-  };
-}
-
-function randomizePieces(){
-  var i;
-  for(i = 0; i < pieces.length; i++){
-    pieces[i].elm.style.left = distribute.l + distribute.w * Math.random() + 'px';
-    pieces[i].elm.style.top = distribute.t + distribute.h * Math.random() + 'px';
-  }
-}
-
-// mouse listeners
-function mouseDown(e){
-  if(carriedPiece){
-    return;
-  }
-  if(e.pageX&&e.pageY){
-    var mouseX = e.pageX - mouseRange.l,mouseY = e.pageY - mouseRange.t;
-    if(mouseX>=0&&mouseX<=mouseRange.w&&mouseY>=0&&mouseY<=mouseRange.h){
-      var piece,hZi=null,hZ=null;
-      for(var i = 0;i<pieces.length;i++){
-        piece = pieces[i];
-        var iX = mouseX - piece.elm.offsetLeft,iY = mouseY -piece.elm.offsetTop;
-        if(iX>=0&&iX<=piece.elm.offsetWidth&&iY>=0&&iY<=piece.elm.offsetHeight){
-          if(hZi===null||piece.elm.style.zIndex>hZ){
-            hZi=i;
-            hZ = piece.elm.style.zIndex;
-          }
-        }
-      }
-      if(hZi!==null){
-        carriedPiece = pieces[hZi];
-        carriedPiece.oX = carriedPiece.elm.offsetLeft;
-        carriedPiece.oY = carriedPiece.elm.offsetTop;
-        carriedPiece.elm.style.zIndex = zCount++;
-        carriedMouseXOffset = mouseX - pieces[hZi].elm.offsetLeft;
-        carriedMouseYOffset = mouseY -pieces[hZi].elm.offsetTop;
-        for(var i =0;i<(ROWS*COLS);i++){
-          if(inmap[i]==carriedPiece.seq) {
-            inmap[i] = null;
-            carriedPiece.oInmap = i;
-            break;
-          }
-        }
-      }
-    }
-  }
-}
-function mouseMove(e){
-  if(carriedPiece&&e.pageX&&e.pageY){
-    var mouseX = e.pageX - mouseRange.l,mouseY = e.pageY - mouseRange.t;
-    if(mouseX>=0&&mouseX<=mouseRange.w&&mouseY>=0&&mouseY<=mouseRange.h)
-    {
-      carriedPiece.elm.style.left = mouseX-carriedMouseXOffset + 'px';
-      carriedPiece.elm.style.top = mouseY-carriedMouseYOffset + 'px';
-    }
-  }
+function imageLoaded(){
 
 }
-function mouseUp(e){
-  if(carriedPiece&&e.pageX&&e.pageY){
-    var mouseX = e.pageX - mouseRange.l,mouseY = e.pageY - mouseRange.t;
-    var picX = mouseX - carriedMouseXOffset, picY = mouseY - carriedMouseYOffset;
-    if(picX>=0&&picX<=mouseRange.w&&picY>=0&&picY<=mouseRange.h)
-    {
-      if(picY<distribute.t){ // 判斷是否需要放到map上
-        var r,c;
-        for(r = 0;r<(ROWS-1);r++) if((r+0.5)*pieceHeight>picY) break;
-        for(c=0;c<(COLS-1);c++) if(((c+0.5)*pieceWidth+mapLeft)>picX) break;
-        if(inmap[r*ROWS+c]!=null){
-          carriedPiece.elm.style.left = carriedPiece.oX + 'px';
-          carriedPiece.elm.style.top = carriedPiece.oY + 'px';
-          inmap[carriedPiece.oInmap] = carriedPiece.seq;
-          carriedPiece.oInmap = null;
-        }else{
-          carriedPiece.elm.style.left = (c*pieceWidth+mapLeft) + 'px';
-          carriedPiece.elm.style.top = r*pieceHeight + 'px';
-          carriedPiece.oInmap = null;
-          inmap[r*ROWS+c] = carriedPiece.seq;
-          updateComplete();
-        }
-      }else{
-        carriedPiece.elm.style.left = picX + 'px';
-        carriedPiece.elm.style.top = picY + 'px';
-      }
-    }else{
-        carriedPiece.elm.style.left = carriedPiece.oX + 'px';
-        carriedPiece.elm.style.top = carriedPiece.oY + 'px';
-    }
-        carriedPiece.oX = null;
-        carriedPiece.oY = null;
-        carriedPiece = null;
-  }
+function win(){
+
 }
-function updateComplete(){
-  var ifwin = true;
-  for(var i =0;i<(ROWS*COLS);i++) { 
-    if(inmap[i]!=i) ifwin = false;
-  }
-  if(ifwin) {
-    setTimeout(win,1);
-  }
-}
-function win(){ //這個方塊可挖空給小隊員填效果
-    console.log('YAYA~~~~~');
-    modifyStatus( '恭喜恭喜～你完成了！' );
-    alert('con');
-}
-function modifyStatus(s){
-    document.getElementById('status').innerHTML = s;
-}
-container.addEventListener('mousedown',mouseDown,false);
-container.addEventListener('mousemove',mouseMove,false);
-container.addEventListener('mouseup',mouseUp,false);
-imageUrlInput.addEventListener('keydown', function(e) {
-  if (e.keyCode === 13) {
-    modifyStatus( 'Loading Picture...' );
-    URL = imageUrlInput.value;
-    image.src = URL;
-  }
-});
+//modifyStatus(s)
+//將傳入的 s 字串，取代 index.html 中 id 為 status 的 <p> 標籤內容。
+//
+//recordImageInfo()
+//記錄圖片的資訊，像是圖片的長跟高，以及切成拼圖碎片的長跟高。
+//
+//setContainerSize()
+//設定 container 這一層的大小，由圖片大小決定，由圖片的長與高乘以一定的比例後得到。
+//setMapSize()
+//設定 map 這一層的大小，由圖片大小決定。因為是要完成拼圖的位置，所以與圖片大小相同。
+//並將它的水平位置移到 container 的正中間。
+//
+//calculateDistributeRegion()
+//計算碎片一開始打亂後能放的位置範圍，網頁上是在 map 下面。
+//同時設定滑鼠可作用的範圍（也就是 container 的範圍）。
+//
+//24generatePieces()
+//「碎片產生機」。把每個碎片產生、顯示出來，並照順序放進 pieces 陣列。
+//
+//randomizePieces()
+//利用 calculateDistributeRegion 所算出的亂數範圍值，將 pieces裡的每一個碎片的位置亂擺在範圍內。
